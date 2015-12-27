7519f06da13d14d9209252cf94b821de6f0b7fd4
andyandy1992 <andrew_smith_92@hotmail.com>
2015-12-27 20:24:52 +0000

    Increased precision to correclty handle extreme cases (e.g. 6 0 0 0) and corrected odds "fraction" representation.

--- horserace_compute_odds.py
+++ horserace_compute_odds.py
@@ -20,7 +20,7 @@ def perm(n, x):
     f = math.factorial
     return Decimal(f(n) / f(n-x))
 
-def round_to(n, precision=0.05):
+def round_to(n, precision=0.005):
     """ Rounds a given number to the nearest 'precision' """
     correction = 0.5 if n >= 0 else -0.5 # so int (i.e. floor rounds to correct)
     return int( n/Decimal(precision)+Decimal(correction) ) * precision
@@ -86,17 +86,28 @@ if __name__ == "__main__":
     pC = prob_win(N, "C", rC, rH, rD, rS)
     pD = prob_win(N, "D", rD, rH, rC, rS)
     pS = prob_win(N, "S", rS, rH, rC, rD)
-
-#    print "Probability of H win="+str(pH)+", C win="+str(pC)+", D win="+str(pD)+", S win="+str(pS)
+    print "Exact winning probabilities: H="+str(pH)+", C="+str(pC)+", D="+str(pD)+", S="+str(pS)
+	#Improve readability:
+    pretty_pH = Fraction(round_to(pH)).limit_denominator()
+    pretty_pC = Fraction(round_to(pC)).limit_denominator()
+    pretty_pD = Fraction(round_to(pD)).limit_denominator()
+    pretty_pS = Fraction(round_to(pS)).limit_denominator()
+    print "Winnings probabilities: H="+str(pretty_pH)+", C="+str(pretty_pC)+", D="+str(pretty_pD)+", S="+str(pretty_pS)
+    #Check probs sum to 1
     total_sum = pH+pC+pD+pS
     if 1-total_sum > 0.05:
         print "ERROR: Probabilities didn't sum to 1. total_sum="+str(total_sum)
 
     #Compute horse odds (formula from https://www.google.co.uk/search?q=convert+probabilities+to+odds&oq=convert+probabilities+to+&gs_l=serp.3.0.0i22i30l10.80129.87682.0.88889.18.17.0.0.0.0.109.1340.16j1.17.0....0...1c.1.64.serp..9.9.731.TNXR5ahlXlk)
-    odds_H = Fraction(round_to(pH/(1-pH))).limit_denominator()
-    odds_C = Fraction(round_to(pC/(1-pC))).limit_denominator()
-    odds_D = Fraction(round_to(pD/(1-pD))).limit_denominator()
-    odds_S = Fraction(round_to(pS/(1-pS))).limit_denominator()
-    print "Odds (e.g. 7/10='10 to 7 against'=stake 7 to win 10):"
-    print "H win="+str(odds_H)+", C win="+str(odds_C)+", D win="+str(odds_D)+", S win="+str(odds_S)
+    #oddsH = (1-pH)/pH
+    #oddsC = (1-pC)/pC
+    #oddsD = {1-pD}/pD
+    #oddsS = (1-pS)/pS
+    pretty_oddsH = (1-pretty_pH)/pretty_pH
+    pretty_oddsC = (1-pretty_pC)/pretty_pC
+    pretty_oddsD = (1-pretty_pD)/pretty_pD
+    pretty_oddsS = (1-pretty_pS)/pretty_pS
     
+    print "Winnning odds (e.g. 3/1:='3 to 1 against'='stake 1 to win 3'):"
+    #print "Exact: H="+str(oddsH)+", C="+str(oddsC)+", D="+str(oddsD)+", S="+str(oddsS)
+    print "H="+str(pretty_oddsH.numerator)+"/"+str(pretty_oddsH.denominator)+", C="+str(pretty_oddsC.numerator)+"/"+str(pretty_oddsC.denominator)+", D="+str(pretty_oddsD.numerator)+"/"+str(pretty_oddsD.denominator)+", S="+str(pretty_oddsS.numerator)+"/"+str(pretty_oddsS.denominator)
